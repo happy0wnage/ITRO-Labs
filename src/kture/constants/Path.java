4ae1f4378b944b53e05320b16aefac91984a084a
Владислав <happy0wnage@gmail.com>
2015-10-16 19:02:32 +0300

    second commit

--- src/kture/constants/Path.java
+++ src/kture/constants/Path.java
@@ -9,5 +9,5 @@ public class Path {
     public static final String KEYBOARD = "../web/xml/keyboard.xml";
     public static final String MOUSE = "../web/xml/mouse.xml";
     public static final String PERIPHERIAL = "web/xml/peripheral.xml";
-    public static final String PHONE = "D:\\Projects\\ITRO Labs\\web\\xml\\phone.xml";
+    public static final String PHONE = "web/xml/phone.xml";
 }
